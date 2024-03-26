ba52209242c853b4475d9a7916f9aa661276a7d7
yanzhanglovx <yanzhanglovx@gmail.com>
2024-03-26 21:53:22 +0800

    0.0.-1

---
+++ Backend/server.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const axios = require('axios')
+const cors = require('cors');
+require('dotenv').config();
+
+const radius = 1000
+const KEY = process.env.GOOGLE_MAPS_API_KEY
+const keywords = [
+    'Starbucks',
+    'McDonald\'s',
+    'Walmart',
+    'City Supper',
+    'IKEA',
+];
+
+const app = express()
+app.use(cors());
+
+
+function sortByDistance(results) {
+    return results.sort((a, b) => a.geometry.location.distance - b.geometry.location.distance);
+}
+
+app.get('/search', async (req, res) => {
+    try {
+        const { latitude, longitude } = req.query;
+        const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json`;
+        for (const keyword of keywords) {
+            const params = {
+                location: `${latitude},${longitude}`,
+                radius: radius,
+                key: KEY,
+                keyword: keyword,
+                rankedby: 'distance'
+            };
+            console.log(`拼接URL：${url}?location=${params.location}&radius=${params.radius}&keyword=${params.keyword}&key=${params.key}`)
+        }
+        console.log('\n')
+
+        // for (const keyword of keywords) {
+        //     const params = {
+        //         location: `${latitude},${longitude}`,
+        //         radius: radius,
+        //         key: KEY,
+        //         type: keyword, // 注意：这里使用type参数来指定查询的店铺类型  
+        //         rankedby: 'distance'
+        //     };
+
+        //     const response = await axios.get(baseURL, { params });
+
+        //     if (response.data.results && response.data.results.length > 0) {
+        //         const filteredResults = response.data.results.filter(place => {
+        //             return place.opening_hours && place.opening_hours.open_now; // 只返回营业的地点
+        //         });
+        //         allResults.push(...filteredResults);
+        //     }
+        // }
+
+        // const sortedResults = sortByDistance(allResults);
+
+        // res.json(sortedResults);
+        // console.log(sortedResults);
+
+        res.json([])
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('An error occurred');
+    }
+
+
+});
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
