a7a72a4708f1be9f591eb865e16ed98442f7977a
xxLovy <1xxlovyxx1@gmail.com>
2024-03-30 21:45:53 +0800

    Add IP limitation and total calling limitation

--- Backend/server.js
+++ Backend/server.js
@@ -52,13 +52,14 @@ async function checkAPIKeyLimit() {
         if (existingRecord) {
             existingRecord.count += keywords.length;
             if (existingRecord.count > DAILY_LIMIT) {
+                console.log(`day:${today}, used:${existingRecord.count}, limit:${DAILY_LIMIT}`)
                 return false;
             }
             await existingRecord.save();
         } else {
             await Usage.create({ date: today, count: keywords.length });
         }
-
+        console.log(`day:${today}, used:${existingRecord.count}, limit:${DAILY_LIMIT}`)
         return true;
     } catch (error) {
         console.error("Error while checking API key limit:", error);
