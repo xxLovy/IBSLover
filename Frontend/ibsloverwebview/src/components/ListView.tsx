88b3a636f125cc0ac4ac5747207f3bdc8402ad38
xxLovy <1xxlovyxx1@gmail.com>
2024-06-11 00:38:13 +0800

    fix bug: cannot build

--- Frontend/ibsloverwebview/src/components/ListView.tsx
+++ Frontend/ibsloverwebview/src/components/ListView.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React, { useEffect, useState } from 'react';
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { useAppDispatch, useAppSelector } from '@/redux/hooks';
