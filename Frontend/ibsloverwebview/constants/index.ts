77e786897941655ada32f2fe60c21d853042f97e
xxLovy <1xxlovyxx1@gmail.com>
2024-06-10 21:14:54 +0800

    fix bug: cannot build

--- Frontend/ibsloverwebview/constants/index.ts
+++ Frontend/ibsloverwebview/constants/index.ts
@@ -46,227 +46,227 @@ export const sidebarItems = [
 ]
 
 
-export const dummyToilets: Toilet[] = [
-    {
-        location: {
-            type: "Point",
-            coordinates: [102, 0.5]
-        },
-        features: {
-            women: "yes",
-            men: "no",
-            accessible: "dontknow",
-            children: "no",
-            free: "yes",
-            fee: 0,
-            genderNeutral: "dontknow"
-        },
-        _id: "664b0c33ea87814171f29706",
-        name: "Toilet 1",
-        description: "Description for Toilet 1",
-        lastUpdateTime: "2024-05-20T08:39:15.996Z",
-        openingHours: new Date("2024-05-20T08:39:15.996Z"), // 转换为 Date 对象
-        isOpening: true,
-        isRemoved: false,
-        votesCount: 5,
-        isFromUser: true,
-        keyword: "clean",
-        users: ["user1"],
-        removeMsg: "Not removed",
-    },
-    {
-        location: {
-            type: "Point",
-            coordinates: [103, 1.5]
-        },
-        features: {
-            women: "no",
-            men: "yes",
-            accessible: "dontknow",
-            children: "yes",
-            free: "no",
-            fee: 2,
-            genderNeutral: "dontknow"
-        },
-        _id: "664b0c34ea87814171f29707",
-        name: "Toilet 2",
-        description: "Description for Toilet 2",
-        lastUpdateTime: "2024-05-20T08:39:15.996Z",
-        openingHours: new Date("2024-05-20T08:39:15.996Z"), // 转换为 Date 对象
-        isOpening: false,
-        isRemoved: false,
-        votesCount: 10,
-        isFromUser: true,
-        keyword: "busy",
-        users: ["user2"],
-        removeMsg: "Not removed",
-    },
-    {
-        location: {
-            type: "Point",
-            coordinates: [120.153576, 30.287459]
-        },
-        features: {
-            women: "yes",
-            men: "yes",
-            accessible: "yes",
-            children: "yes",
-            free: "yes",
-            genderNeutral: "dontknow"
-        },
-        _id: "664b122591ac3fac2a595bbd",
-        name: "Updated Public Toilet",
-        description: "Updated description",
-        lastUpdateTime: "2024-05-20T09:23:53.173Z",
-        openingHours: new Date("2024-05-20T09:23:53.173Z"), // 转换为 Date 对象
-        isOpening: true,
-        isRemoved: false,
-        votesCount: 1,
-        isFromUser: true,
-        users: [
-            "664b0c3e4bbc29fd613b8342",
-            "664b0c3e4bbc29fd613b8342",
-            "664b0c3e4bbc29fd613b8342"
-        ],
-    },
-    {
-        location: {
-            type: "Point",
-            coordinates: [102, 0.5]
-        },
-        features: {
-            women: "yes",
-            men: "no",
-            accessible: "dontknow",
-            children: "no",
-            free: "yes",
-            fee: 0,
-            genderNeutral: "dontknow"
-        },
-        _id: "664b0c33ea87814171f29706",
-        name: "Toilet 1",
-        description: "Description for Toilet 1",
-        lastUpdateTime: "2024-05-20T08:39:15.996Z",
-        openingHours: new Date("2024-05-20T08:39:15.996Z"), // 转换为 Date 对象
-        isOpening: true,
-        isRemoved: false,
-        votesCount: 5,
-        isFromUser: true,
-        keyword: "clean",
-        users: ["user1"],
-        removeMsg: "Not removed",
-    },
-    {
-        location: {
-            type: "Point",
-            coordinates: [103, 1.5]
-        },
-        features: {
-            women: "no",
-            men: "yes",
-            accessible: "dontknow",
-            children: "yes",
-            free: "no",
-            fee: 2,
-            genderNeutral: "dontknow"
-        },
-        _id: "664b0c34ea87814171f29707",
-        name: "Toilet 2",
-        description: "Description for Toilet 2",
-        lastUpdateTime: "2024-05-20T08:39:15.996Z",
-        openingHours: new Date("2024-05-20T08:39:15.996Z"), // 转换为 Date 对象
-        isOpening: false,
-        isRemoved: false,
-        votesCount: 10,
-        isFromUser: true,
-        keyword: "busy",
-        users: ["user2"],
-        removeMsg: "Not removed",
-    },
-    {
-        location: {
-            type: "Point",
-            coordinates: [120.153576, 30.287459]
-        },
-        features: {
-            women: "yes",
-            men: "yes",
-            accessible: "yes",
-            children: "yes",
-            free: "yes",
-            genderNeutral: "dontknow"
-        },
-        _id: "664b122591ac3fac2a595bbd",
-        name: "Updated Public Toilet",
-        description: "Updated description",
-        lastUpdateTime: "2024-05-20T09:23:53.173Z",
-        openingHours: new Date("2024-05-20T09:23:53.173Z"), // 转换为 Date 对象
-        isOpening: true,
-        isRemoved: false,
-        votesCount: 1,
-        isFromUser: true,
-        users: [
-            "664b0c3e4bbc29fd613b8342",
-            "664b0c3e4bbc29fd613b8342",
-            "664b0c3e4bbc29fd613b8342"
-        ],
-    },
-    {
-        location: {
-            type: "Point",
-            coordinates: [-122.4191, 37.7751] // 经度, 纬度
-        },
-        features: {
-            women: "yes",
-            men: "no",
-            accessible: "dontknow",
-            children: "no",
-            free: "yes",
-            fee: 0,
-            genderNeutral: "dontknow"
-        },
-        _id: "664b0c33ea87814171f29706",
-        name: "Toilet 1",
-        description: "Description for Toilet 1",
-        lastUpdateTime: "2024-05-20T08:39:15.996Z",
-        openingHours: new Date("2024-05-20T08:39:15.996Z"), // 转换为 Date 对象
-        isOpening: true,
-        isRemoved: false,
-        votesCount: 5,
-        isFromUser: true,
-        keyword: "clean",
-        users: ["user1"],
-        removeMsg: "Not removed",
-    },
-    {
-        location: {
-            type: "Point",
-            coordinates: [-122.4196, 37.7748]
-        },
-        features: {
-            women: "no",
-            men: "yes",
-            accessible: "dontknow",
-            children: "yes",
-            free: "no",
-            fee: 2,
-            genderNeutral: "dontknow"
-        },
-        _id: "664b0c34ea87814171f29707",
-        name: "Toilet 2",
-        description: "Description for Toilet 2",
-        lastUpdateTime: "2024-05-20T08:39:15.996Z",
-        openingHours: new Date("2024-05-20T08:39:15.996Z"), // 转换为 Date 对象
-        isOpening: false,
-        isRemoved: false,
-        votesCount: 10,
-        isFromUser: true,
-        keyword: "busy",
-        users: ["user2"],
-        removeMsg: "Not removed",
-    }
+// export const dummyToilets: Toilet[] = [
+//     {
+//         location: {
+//             type: "Point",
+//             coordinates: [102, 0.5]
+//         },
+//         features: {
+//             women: "yes",
+//             men: "no",
+//             accessible: "dontknow",
+//             children: "no",
+//             free: "yes",
+//             fee: 0,
+//             genderNeutral: "dontknow"
+//         },
+//         _id: "664b0c33ea87814171f29706",
+//         name: "Toilet 1",
+//         description: "Description for Toilet 1",
+//         lastUpdateTime: "2024-05-20T08:39:15.996Z",
+//         openingHours: new Date("2024-05-20T08:39:15.996Z"), // 转换为 Date 对象
+//         isOpening: true,
+//         isRemoved: false,
+//         votesCount: 5,
+//         isFromUser: true,
+//         keyword: "clean",
+//         users: ["user1"],
+//         removeMsg: "Not removed",
+//     },
+//     {
+//         location: {
+//             type: "Point",
+//             coordinates: [103, 1.5]
+//         },
+//         features: {
+//             women: "no",
+//             men: "yes",
+//             accessible: "dontknow",
+//             children: "yes",
+//             free: "no",
+//             fee: 2,
+//             genderNeutral: "dontknow"
+//         },
+//         _id: "664b0c34ea87814171f29707",
+//         name: "Toilet 2",
+//         description: "Description for Toilet 2",
+//         lastUpdateTime: "2024-05-20T08:39:15.996Z",
+//         openingHours: new Date("2024-05-20T08:39:15.996Z"), // 转换为 Date 对象
+//         isOpening: false,
+//         isRemoved: false,
+//         votesCount: 10,
+//         isFromUser: true,
+//         keyword: "busy",
+//         users: ["user2"],
+//         removeMsg: "Not removed",
+//     },
+//     {
+//         location: {
+//             type: "Point",
+//             coordinates: [120.153576, 30.287459]
+//         },
+//         features: {
+//             women: "yes",
+//             men: "yes",
+//             accessible: "yes",
+//             children: "yes",
+//             free: "yes",
+//             genderNeutral: "dontknow"
+//         },
+//         _id: "664b122591ac3fac2a595bbd",
+//         name: "Updated Public Toilet",
+//         description: "Updated description",
+//         lastUpdateTime: "2024-05-20T09:23:53.173Z",
+//         openingHours: new Date("2024-05-20T09:23:53.173Z"), // 转换为 Date 对象
+//         isOpening: true,
+//         isRemoved: false,
+//         votesCount: 1,
+//         isFromUser: true,
+//         users: [
+//             "664b0c3e4bbc29fd613b8342",
+//             "664b0c3e4bbc29fd613b8342",
+//             "664b0c3e4bbc29fd613b8342"
+//         ],
+//     },
+//     {
+//         location: {
+//             type: "Point",
+//             coordinates: [102, 0.5]
+//         },
+//         features: {
+//             women: "yes",
+//             men: "no",
+//             accessible: "dontknow",
+//             children: "no",
+//             free: "yes",
+//             fee: 0,
+//             genderNeutral: "dontknow"
+//         },
+//         _id: "664b0c33ea87814171f29706",
+//         name: "Toilet 1",
+//         description: "Description for Toilet 1",
+//         lastUpdateTime: "2024-05-20T08:39:15.996Z",
+//         openingHours: new Date("2024-05-20T08:39:15.996Z"), // 转换为 Date 对象
+//         isOpening: true,
+//         isRemoved: false,
+//         votesCount: 5,
+//         isFromUser: true,
+//         keyword: "clean",
+//         users: ["user1"],
+//         removeMsg: "Not removed",
+//     },
+//     {
+//         location: {
+//             type: "Point",
+//             coordinates: [103, 1.5]
+//         },
+//         features: {
+//             women: "no",
+//             men: "yes",
+//             accessible: "dontknow",
+//             children: "yes",
+//             free: "no",
+//             fee: 2,
+//             genderNeutral: "dontknow"
+//         },
+//         _id: "664b0c34ea87814171f29707",
+//         name: "Toilet 2",
+//         description: "Description for Toilet 2",
+//         lastUpdateTime: "2024-05-20T08:39:15.996Z",
+//         openingHours: new Date("2024-05-20T08:39:15.996Z"), // 转换为 Date 对象
+//         isOpening: false,
+//         isRemoved: false,
+//         votesCount: 10,
+//         isFromUser: true,
+//         keyword: "busy",
+//         users: ["user2"],
+//         removeMsg: "Not removed",
+//     },
+//     {
+//         location: {
+//             type: "Point",
+//             coordinates: [120.153576, 30.287459]
+//         },
+//         features: {
+//             women: "yes",
+//             men: "yes",
+//             accessible: "yes",
+//             children: "yes",
+//             free: "yes",
+//             genderNeutral: "dontknow"
+//         },
+//         _id: "664b122591ac3fac2a595bbd",
+//         name: "Updated Public Toilet",
+//         description: "Updated description",
+//         lastUpdateTime: "2024-05-20T09:23:53.173Z",
+//         openingHours: new Date("2024-05-20T09:23:53.173Z"), // 转换为 Date 对象
+//         isOpening: true,
+//         isRemoved: false,
+//         votesCount: 1,
+//         isFromUser: true,
+//         users: [
+//             "664b0c3e4bbc29fd613b8342",
+//             "664b0c3e4bbc29fd613b8342",
+//             "664b0c3e4bbc29fd613b8342"
+//         ],
+//     },
+//     {
+//         location: {
+//             type: "Point",
+//             coordinates: [-122.4191, 37.7751] // 经度, 纬度
+//         },
+//         features: {
+//             women: "yes",
+//             men: "no",
+//             accessible: "dontknow",
+//             children: "no",
+//             free: "yes",
+//             fee: 0,
+//             genderNeutral: "dontknow"
+//         },
+//         _id: "664b0c33ea87814171f29706",
+//         name: "Toilet 1",
+//         description: "Description for Toilet 1",
+//         lastUpdateTime: "2024-05-20T08:39:15.996Z",
+//         openingHours: new Date("2024-05-20T08:39:15.996Z"), // 转换为 Date 对象
+//         isOpening: true,
+//         isRemoved: false,
+//         votesCount: 5,
+//         isFromUser: true,
+//         keyword: "clean",
+//         users: ["user1"],
+//         removeMsg: "Not removed",
+//     },
+//     {
+//         location: {
+//             type: "Point",
+//             coordinates: [-122.4196, 37.7748]
+//         },
+//         features: {
+//             women: "no",
+//             men: "yes",
+//             accessible: "dontknow",
+//             children: "yes",
+//             free: "no",
+//             fee: 2,
+//             genderNeutral: "dontknow"
+//         },
+//         _id: "664b0c34ea87814171f29707",
+//         name: "Toilet 2",
+//         description: "Description for Toilet 2",
+//         lastUpdateTime: "2024-05-20T08:39:15.996Z",
+//         openingHours: new Date("2024-05-20T08:39:15.996Z"), // 转换为 Date 对象
+//         isOpening: false,
+//         isRemoved: false,
+//         votesCount: 10,
+//         isFromUser: true,
+//         keyword: "busy",
+//         users: ["user2"],
+//         removeMsg: "Not removed",
+//     }
 
-];
+// ];
 
 export const checkboxItems = [
     {
