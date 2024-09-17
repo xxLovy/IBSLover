cddfead800ead9f6ebfc3c4ec04ce2ea4334a3e6
xxLovy <1xxlovyxx1@gmail.com>
2024-09-17 14:48:48 -0700

    delete Starbucks

--- Backend/IBSLoverBackend/src/constants/index.ts
+++ Backend/IBSLoverBackend/src/constants/index.ts
@@ -6,7 +6,7 @@ export const IP_REQUEST_EXPIRE = 10;
 export const DAILY_LIMIT = 1000;
 export const MAX_DISTANCE = 20;
 export const keywords = [
-    'Starbucks',
+    // 'Starbucks',
     // 'McDonald\'s',
     'Walmart',
     // 'City Supper',
